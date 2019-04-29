4072d016642e2244c2ca6c72335af3096a8ebc98
IanBrison <ianbrison222@gmail.com>
2019-04-29 13:29:47 +0900

    add a fullset usecase

---
+++ Test/Unit/Service/Usecase/PrepareTopPageTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Test\Unit\Service\Usecase;
+
+use App\Service\Usecase\PrepareTopPage;
+use App\Service\UsecaseInput\PrepareTopPageInput;
+use App\Service\UsecaseOutput\PrepareTopPageOutput;
+use Core\Di\DiContainer as Di;
+use Test\TestCase;
+
+class PrepareTopPageTest extends TestCase {
+
+	/**
+	 * @test
+	 */
+	public function execute() {
+		$input = \Mockery::mock(PrepareTopPageInput::class);
+		$output = \Mockery::mock(PrepareTopPageOutput::class);
+		Di::set(PrepareTopPageOutput::class, $output);
+
+		$usecase = new PrepareTopPage($input);
+		$presenter = $usecase->execute();
+		$this->assertSame($output, $presenter);
+	}
+}
